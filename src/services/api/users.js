2587ece7c108b407ecb770b8fd1f3713b24be84e
Samina Angelo <komlanvi---angelo.samina@etudiant.univ-reims.fr>
2023-04-06 13:38:30 +0200

    Modification de l'url

--- src/services/api/users.js
+++ src/services/api/users.js
@@ -1,13 +1,13 @@
 import React, { createContext } from "react";
 
-export const BASE_URL = "https://127.0.0.1:8000/api";
+export const BASE_URL = "https://127.0.0.1:8000";
 
 export const UserContext = React.createContext({});
 
 
 
 export function getMe() {
-    return fetch(`${BASE_URL}/me`, { credentials: "include" })
+    return fetch(`${BASE_URL}/api/me`, { credentials: "include" })
         .then((response) => {
             if (response.ok) {
                 return response.json();
