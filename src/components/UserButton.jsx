ba017079b34bd85d78f34640ca522207f9eee4f7
Samina Angelo <komlanvi---angelo.samina@etudiant.univ-reims.fr>
2023-04-06 15:34:42 +0200

    Ajout de l'inscription

--- src/components/UserButton.jsx
+++ src/components/UserButton.jsx
@@ -2,27 +2,31 @@ import React, { useContext } from "react";
 import { UserContext } from "../contexts/user/index";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faKey, faSpinner } from "@fortawesome/free-solid-svg-icons";
-import { loginUrl, logoutUrl } from "../services/api/users";
+import { loginUrl, logoutUrl, registerUrl } from "../services/api/users";
 
-function UserButton()
-{
+function UserButton() {
     const dataUser = useContext(UserContext);
-    if (dataUser['userData'] === undefined)
-    {
-        return <FontAwesomeIcon icon={faSpinner} className="foldable-icon"/>
+  
+    if (dataUser['userData'] === undefined) {
+      return <FontAwesomeIcon icon={faSpinner} className="foldable-icon" />;
+    } else if (dataUser['userData'] === null) {
+      return (
+        <>
+          <a href={loginUrl()}>
+            Connexion
+          </a>
+          <a href={registerUrl()}>
+            Inscription
+          </a>
+        </>
+      );
+    } else {
+      return (
+        <a href={logoutUrl()}>
+          Deconnexion
+        </a>
+      );
     }
-    else if (dataUser['userData'] === null)
-    {
-        return  <a href={loginUrl()}>
-                        <FontAwesomeIcon icon={faKey} className="foldable-icon"/>
-                </a>
-    }
-    else
-    {
-        return <a href={logoutUrl()}>
-                        Deconnexion
-                </a>
-    }
-}
-
-export default UserButton;
+  }
+  
+  export default UserButton;
