83ceae8938abd3def7ecfb470100958b6c77c569
Luc <luc.schoeny@etudiant.univ-reims.fr>
2023-04-07 02:48:17 +0200

    creation d'un message d'erreur si webcam non trouver

--- src/components/Qrcode.jsx
+++ src/components/Qrcode.jsx
@@ -1,14 +1,16 @@
+import React, { useState } from 'react';
 import QrScanner from 'react-qr-scanner';
-
+import '../Styles/qrcode.css';
 
 function MyComponent() {
+    const [error, setError] = useState(false);
 
     const getCruBySlug = (slug) => {
         const query = `?name=${encodeURIComponent(slug)}`;
         const url = `http://localhost:8000/api/cruses${query}`;
-        return fetch(url,  {credentials: "include",method: "GET"}).then((response) => response.json());
+        return fetch(url, { credentials: "include", method: "GET" }).then((response) => response.json());
     };
-    const  result = getCruBySlug();
+    const result = getCruBySlug();
 
     const handleScan = (data) => {
         if (data) {
@@ -18,7 +20,7 @@ function MyComponent() {
                 getCruBySlug(urls)
                     .then((req) => {
                         setResult(req[0]);
-                        window.location.href = "http://localhost:5173/vignoble/%22"+req[0].id;
+                        window.location.href = "http://localhost:5173/vignoble/%22" + req[0].id;
                     })
                     .catch(() => {
                         /*setError(true);*/
@@ -29,21 +31,26 @@ function MyComponent() {
 
     const handleError = (error) => {
         console.log(error);
-    }
-
+        setError(true);
+    };
 
     return (
         <div className="App">
-            <QrScanner
-                id={"camera"}
-                delay={100}
-                onError={handleError}
-                onScan={handleScan}
-                facingMode="rear"
-            />
+            {error ? (
+                <div className="error-message">
+                    L'accès à la caméra est impossible. Veuillez vérifier les permissions de la caméra.
+                </div>
+            ) : (
+                <QrScanner
+                    id={"camera"}
+                    delay={100}
+                    onError={handleError}
+                    onScan={handleScan}
+                    facingMode="rear"
+                />
+            )}
         </div>
-    )
+    );
 }
 
-
-export default MyComponent
+export default MyComponent;
